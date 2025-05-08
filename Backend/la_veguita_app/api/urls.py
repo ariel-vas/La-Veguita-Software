771f41a76df0adbf3e3494501cbe2bd23f261f84
Fernando Iribarra <fernando.iribarra@usach.cl>
2025-05-08 00:34:01 -0400

    Se finaliza creacion de CRUD Product. Related to #74

--- Backend/la_veguita_app/api/urls.py
+++ Backend/la_veguita_app/api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("users/", views.UserListCreate.as_view(), name="user-view-create-list"),
-    path("users/<int:id>", views.UserRetrieveUpdateDestroy.as_view(), name="user-view-retrieve-update-destroy")
+    path("users/<int:id>", views.UserRetrieveUpdateDestroy.as_view(), name="user-view-retrieve-update-destroy"),
+    path("product/", views.ProductListCreate.as_view(), name="product-view-create-list"),
+    path("product/<int:id_product>", views.ProductRetrieveUpdateDestroy.as_view(), name="product-view-retrieve-update-destroy"),
 ]
