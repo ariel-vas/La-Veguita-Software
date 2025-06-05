5e769a1157cf6dcc026c2bb295d331eccf7b4554
Fernando Iribarra <fernando.iribarra@usach.cl>
2025-06-05 04:35:44 -0400

    Se habilitan endpoints de Backend para el procesamiento de archivos PDF de productos y categorias de Zpos. Se modifica modelo de productos. Se agrega nueva dependencia pdfplumber.

--- Backend/la_veguita_app/api/models/product.py
+++ Backend/la_veguita_app/api/models/product.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, RegexValidator
 from .category import Category
 from .supplier import Supplier
 
@@ -9,20 +9,29 @@ class Product(models.Model):
         UNIT = 'unit'
         KILO = 'kilo'
 
-    id_product = models.DecimalField(max_digits=50, decimal_places=0, unique=True, primary_key=True, validators=[MinValueValidator(0)])
-    name = models.CharField(max_length=150, unique=True)
-    description = models.TextField()
+    id_product = models.CharField(
+        max_length=50,
+        primary_key=True,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\d+$',
+                message='id_product must contain only digits.'
+            )
+        ]
+    )
+    description = models.CharField(max_length=150)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
-    sale_price_unit = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
-    sale_price_kilo = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     wholesale_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     wholesale_quantity = models.DecimalField(max_digits=12, decimal_places=4, validators=[MinValueValidator(0)])
     discount_surcharge = models.DecimalField(max_digits=5, decimal_places=2)
     stock = models.DecimalField(max_digits=12, decimal_places=4, validators=[MinValueValidator(0)])
     critical_stock = models.DecimalField(max_digits=12, decimal_places=4, validators=[MinValueValidator(0)])
-    entry_stock_unit = models.CharField(max_length=4, choices=StockUnit.choices)
-    exit_stock_unit = models.CharField(max_length=4, choices=StockUnit.choices)
-    composed_product = models.BooleanField()
+    entry_stock_unit = models.CharField(max_length=4, choices=StockUnit.choices)  # App exclusive attribute
+    exit_stock_unit = models.CharField(max_length=4, choices=StockUnit.choices)  # App exclusive attribute
+    composed_product = models.BooleanField(default=False)  # App exclusive attribute
+    checked = models.BooleanField(default=False)  # Dinamically toggled through frontend
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='products', null=True, blank=True)
     supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, related_name='products', null=True, blank=True)
 
